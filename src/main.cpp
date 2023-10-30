ffd19d8fa0c261b9684a250c147619e1d55d1ee9
bee <bee@macbook.local>
2023-10-29 23:37:06 -0300

    up 010

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ author : amaya
 
 //#include <run.h>
 #include<spi.h>
-
+#include <memory>
 int main(){
-auto _spi{std::make_unique<SPI::Spi>()}};
+std::unique_ptr<SPI::Spi> _spi { std::make_unique<SPI::Spi>() };
 //auto run { std::make_unique<MRF24J40::Run>()};
 
 return 0 ;
