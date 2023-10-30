89a096a126d8326483aa6d4313f0cd32fdaf8329
bee <bee@macbook.local>
2023-10-29 22:46:52 -0300

    up 010

--- libs/src/spi.h
+++ libs/src/spi.h
@@ -45,8 +45,8 @@ namespace SPI{
     
   private:
 
-    uint8_t tx_buffer[4];
-    uint8_t rx_buffer[4];
+    uint8_t tx_buffer[4]{};
+    uint8_t rx_buffer[4]{};
 
     const uint32_t len_data = 32;
     const uint32_t spi_speed = 5000000;
