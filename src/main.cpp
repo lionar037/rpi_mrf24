e417ccdf481f546e845286fbc50d2eb88affd827
bee <bee@macbook.local>
2023-10-30 01:35:43 -0300

    up 010

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ author : amaya
 #include <memory>
 int main(){
 std::unique_ptr<SPI::Spi> spi { std::make_unique<SPI::Spi>() };
-
+spi->Transfer3bytes(0x00E0C1);
+spi->Transfer3bytes(0x00C1E0);
 return 0 ;
 }
