78f6e6c1bbe0d041a7b2873eecf8f8ad7ac2292c
bee <bee@macbook.local>
2023-11-01 20:12:35 -0300

    update

--- libs/src/run.cpp
+++ libs/src/run.cpp
@@ -38,7 +38,7 @@ void Run::loop() {
         last_time = current_time;
         //std::cout<<"txxxing...\n";
         std::cout<<"send16 () ... \n";
-        mrf24j40_spi.send16(ADDR_SLAVE, "@");//send data//original//mrf24j40_spi.send16(0x4202, "abcd")
+        mrf24j40_spi.send16(ADDR_SLAVE, "@aA");//send data//original//mrf24j40_spi.send16(0x4202, "abcd")
     }
 }
 
