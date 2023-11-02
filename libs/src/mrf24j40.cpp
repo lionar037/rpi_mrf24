b71f081087f524aec295afe8e44854be33b5c17b
bee <bee@macbook.local>
2023-11-01 21:46:54 -0300

    update

--- libs/src/mrf24j40.cpp
+++ libs/src/mrf24j40.cpp
@@ -19,7 +19,7 @@ namespace MRF24J40{
         std::cout <<"Mrf24j( )\r\n";
     }
 
-    uint8_t Mrf24j::read_short(uint8_t address) {
+    const uint8_t Mrf24j::read_short(uint8_t address) {
             // 0 top for short addressing, 0 bottom for read
         const uint16_t tmp = (address<<1 & 0b01111110) & 0x00ff;
         const uint8_t ret = prt_spi->Transfer2bytes(tmp); // envia 16 , los mas significativos en 0x00 , los menos significativos envia el comando
@@ -34,7 +34,7 @@ printf("2:0x%x ",ret);
         return;
     }
 
-    uint8_t Mrf24j::read_long(const uint16_t address) {
+    const uint8_t Mrf24j::read_long(const uint16_t address) {
        const  uint8_t lsb_adress = (address >> 3 )& 0x7F;
        const  uint8_t msb_adress = (address << 5) & 0xE0;
      const   uint32_t tmp = (( 0x00 << 16 ) | (0x80 | lsb_adress) | (msb_adress <<8) ) &  0x00ffffff;
