d2bea7b8ee558363843c886850f2769b420b16b0
bee <bee@macbook.local>
2023-11-07 23:51:06 -0300

    update 2023

--- libs/src/run.cpp
+++ libs/src/run.cpp
@@ -95,7 +95,7 @@ void handle_rx() {
     const int recevive_data_length = mrf24j40_spi.rx_datalength();
     std::cout << "\t\tdata_length : "<<std::dec<< recevive_data_length<<"\n\t";
 
-    for (int i = 0; i < recevive_data_length; i++) {
+    for (int i = -11; i < recevive_data_length; i++) {
         //std::cout<<std::hex<<mrf24j40_spi.get_rxinfo()->rx_data[i];
         printf("0x%x ",mrf24j40_spi.get_rxinfo()->rx_data[i]);
         //std::cout<<".";
