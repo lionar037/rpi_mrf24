f9dd47bc69384cb4b28bc218492c4555045658ef
bee <bee@macbook.local>
2023-11-09 00:57:13 -0300

    update

--- libs/src/radioAddress.h
+++ libs/src/radioAddress.h
@@ -6,12 +6,12 @@
 #define MY_LONG_ADDRESS 	(0x0123456789abcdefull)		// device MAC address (8 bytes, 64 bit)
 #define MY_SHORT_ADDRESS	(0xaa55)					// short (2 byte) 802.15.4 address
 #define MY_PAN_ID			(0x0f00)					// PAN identifier
-
+/*
 #define PACKET_BUFFERS 0x01
 #define RX_BUFFER_SIZE 0x01
 #define WRITE_SOFTRST 0x00
 #define READ_SOFTRST 0x00
-
+*/
 bool RADIO_CS ;
 bool RFIE;
 bool RADIO_TX ;
