be6e466c0a0b781b8c1d6913f6c8c8002cd8156f
bee <bee@macbook.local>
2023-11-22 03:04:12 -0300

    update

--- libs/src/mrf24j40.cpp
+++ libs/src/mrf24j40.cpp
@@ -97,14 +97,14 @@ uint64_t address64 ;
 //for (int i=0 ;i<7;i++)
    // address64 |=  (uint64_t)(read_short(MRF_EADR0+i))<< i*8;
 
- address64 |= (uint64_t)(read_short(MRF_EADR0)<< 0);
- address64 |= (uint64_t)(read_short(MRF_EADR1)<< 8);
- address64 |= (uint64_t)(read_short(MRF_EADR2)<<16);
- address64 |= (uint64_t)(read_short(MRF_EADR3)<<24);
- address64 |= (uint64_t)(read_short(MRF_EADR4)<<32);
- address64 |= (uint64_t)(read_short(MRF_EADR5)<<40);
- address64 |= (uint64_t)(read_short(MRF_EADR6)<<48);
- address64 |= (uint64_t)(read_short(MRF_EADR7)<<56);
+ address64 |= (uint64_t)(read_short(MRF_EADR0))<< 0;
+ address64 |= (uint64_t)(read_short(MRF_EADR1))<< 8;
+ address64 |= (uint64_t)(read_short(MRF_EADR2))<<16;
+ address64 |= (uint64_t)(read_short(MRF_EADR3))<<24;
+ address64 |= (uint64_t)(read_short(MRF_EADR4))<<32;
+ address64 |= (uint64_t)(read_short(MRF_EADR5))<<40;
+ address64 |= (uint64_t)(read_short(MRF_EADR6))<<48;
+ address64 |= (uint64_t)(read_short(MRF_EADR7))<<56;
 
 return  address64;
 }
