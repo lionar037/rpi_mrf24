464cef9196264c0d3740393037330a6e6f3036f3
bee <bee@macbook.local>
2023-11-22 00:37:36 -0300

    update

--- libs/src/gpio.cpp
+++ libs/src/gpio.cpp
@@ -164,7 +164,7 @@ namespace GPIO{
         }
         else{
             gpio_set_value(gpio_out,VALUE_HIGH);
-             std::this_thread::sleep_for(std::chrono::milliseconds(100));
+             std::this_thread::sleep_for(std::chrono::milliseconds(1500));
         }
            
 
