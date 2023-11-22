6ca546e908ab1b10bab6d92343c8b98f6226ec17
bee <bee@macbook.local>
2023-11-22 01:06:55 -0300

    update

--- libs/src/mrf24j40.cpp
+++ libs/src/mrf24j40.cpp
@@ -93,7 +93,7 @@ uint16_t Mrf24j::address16_read(void) {
 
 uint64_t Mrf24j::address64_read(void){
 uint64_t address64 ;
-for (int i ;i<8;i++)
+for (int i=0 ;i<8;i++)
     address64 |=  (uint64_t)(read_short(MRF_EADR0+i))<< i*8;
 
    // address64 |= (uint64_t)(read_short(MRF_EADR1))<< 8;
