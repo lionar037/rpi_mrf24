1f6f45b1410b4964e50b83d7f4a12e385c7c6afb
bee <bee@macbook.local>
2023-11-25 20:41:29 -0300

    update 202311252041

--- libs/src/database.cpp
+++ libs/src/database.cpp
@@ -68,7 +68,7 @@ namespace DATABASE{
 Database_t::Database_t()
 ://con{std::make_unique<sql::Connection>()} , //driver{std::make_unique<sql::mysql::MySQL_Driver>()}
 driver{sql::mysql::get_mysql_driver_instance()}
-, con{driver->connect("tcp://192.168.1.45:3306", "user1", "passwd");    }
+, con{driver->connect("tcp://192.168.1.45:3306", "user1", "passwd") }
 {
   std::cout<<"Database_t()\n";  
 }
