f2197edfacd11e374fed42e32dcbe4a03a749497
bee <bee@macbook.local>
2023-11-25 20:40:50 -0300

    update 202311252040

--- libs/src/database.cpp
+++ libs/src/database.cpp
@@ -23,7 +23,7 @@ namespace DATABASE{
        // driver = sql::mysql::get_mysql_driver_instance();
        
        // con = driver->connect("tcp://127.0.0.1:3306", "admin", "passwd");
-        con = driver->connect("tcp://192.168.1.45:3306", "user1", "passwd");        
+       // con = driver->connect("tcp://192.168.1.45:3306", "user1", "passwd");        
         // Seleccionar la base de datos
         con->setSchema("databaseMDB");
         // Recuperar el valor del ID 64
@@ -66,8 +66,9 @@ namespace DATABASE{
 
 
 Database_t::Database_t()
-:con{std::make_unique<sql::Connection>()} , //driver{std::make_unique<sql::mysql::MySQL_Driver>()}
+://con{std::make_unique<sql::Connection>()} , //driver{std::make_unique<sql::mysql::MySQL_Driver>()}
 driver{sql::mysql::get_mysql_driver_instance()}
+, con{driver->connect("tcp://192.168.1.45:3306", "user1", "passwd");    }
 {
   std::cout<<"Database_t()\n";  
 }
