2ea034c4a6ecf075c2356252e7c1c176167225b9
bee <bee@macbook.local>
2023-11-25 22:25:56 -0300

    update 202311252225

--- libs/src/gpio.cpp
+++ libs/src/gpio.cpp
@@ -169,7 +169,7 @@ namespace GPIO{
         }
         else{
             gpio_set_value(gpio_out,VALUE_HIGH);
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));            
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));            
         }
          //std::this_thread::sleep_for(std::chrono::milliseconds(100));            
 
