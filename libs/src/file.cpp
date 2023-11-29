1eebaf791bea14e079b756658dac5d412b08bb6e
bee <bee@macbook.local>
2023-11-29 00:14:09 -0300

    update 202311290014

--- libs/src/file.cpp
+++ libs/src/file.cpp
@@ -1,6 +1,14 @@
-#include <file.h>
+
 #include <iostream>
 #include <fstream>
+extern "C"
+{
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <stdint.h>
+}
+
+#include <file.h>
 
 
 namespace FILESYSTEM{
@@ -11,6 +19,23 @@ namespace FILESYSTEM{
 
     File_t::~File_t(){
 
+    }
+
+
+    unsigned char* loadFile(const char* filename){
+
+            std::ifstream file(filename, std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cerr << "Error al abrir el archivo: " << filename << std::endl;
+        return nullptr;
+    }
+
+    packet_mrf24 packet;
+    file.read(reinterpret_cast<char*>(&packet), sizeof(packet_mrf24));
+
+
+        return 0;
     }
 
 }
