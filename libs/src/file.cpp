46ecf492972a593efb1c9c7cdae813ef5ae78188
bee <bee@macbook.local>
2023-11-29 11:35:36 -0300

    update 202311291135

--- libs/src/file.cpp
+++ libs/src/file.cpp
@@ -100,7 +100,9 @@ size_t dataSize = packet.size;
         std::tm localTime = *std::localtime(&currentTime);
 
         // Formatear la fecha y hora según tu especificación
-       auto oss << std::put_time(&localTime, "\n%Y%m%d%H%M%S") << std::endl;
+        std::cout<<"\n";
+        auto oss << std::put_time(&localTime, "%Y%m%d%H%M%S");
+         std::cout<<"\n";
         std::string tyme = oss.str();
         return 0;
     }
