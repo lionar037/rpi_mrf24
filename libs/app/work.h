40ed23f1e75ea376012879f47b4e0a016f6007c2
bee <bee@macbook.local>
2023-11-30 19:19:49 -0300

    update 202311301919

--- libs/app/work.h
+++ libs/app/work.h
@@ -5,7 +5,7 @@ namespace WORK{
 
     struct Work_t{
             virtual bool    create(const char*)=0;  
-            virtual std::string    create(const char*)=0;       
+            virtual std::string    create(const char*);       
             virtual         ~Work_t()=default;
         private:
     };
