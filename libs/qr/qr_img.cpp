9166298e5909476aab709ab9fc7d3c12978d0887
bee <bee@macbook.local>
2023-11-30 02:57:24 -0300

    update 202311300257

--- libs/qr/qr_img.cpp
+++ libs/qr/qr_img.cpp
@@ -98,7 +98,7 @@ void Qr_img_t::saveQRCodeImage(const QRcode* qr, const char* filename) {
 
         // Guarda el código QR como imagen PNG
         //tyme->get_tyme() 
-        const auto file_tmp =reinterpret_cast<char*>( "text "+ "log/qr.png");
+        const auto file_tmp =reinterpret_cast<char*>( "text " + "log/qr.png");
         saveQRCodeImage(qr, file_tmp);
 
         // Libera la memoria
