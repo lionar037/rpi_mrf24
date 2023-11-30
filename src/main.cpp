ac7afc388d9df551375603ccf066beabc092e954
bee <bee@macbook.local>
2023-11-29 22:53:02 -0300

    update 202311292253

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@ author : amaya
 */
 
 
-#include <run.h>
+//#include <run.h>
 #include <memory>
+#include <spi/run.h>
 
 int main(){
     std::unique_ptr<MRF24J40::Run> mrf { std::make_unique<MRF24J40::Run>() };
