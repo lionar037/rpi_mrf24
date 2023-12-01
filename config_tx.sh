bebbfd9797a840a45ba2e1647cb1d54ba1fd708e
bee <bee@macbook.local>
2023-12-01 13:57:03 -0300

    update 202312011357

--- config_tx.sh
+++ config_tx.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-echo "Configuring as a transmitter"
+# Verifica si se proporcionó al menos un argumento
+if [ $# -lt 1 ]; then
+    echo "Uso: $0 <config>"
+    exit 1
+fi
 
-#cp libs/app/config.22h libs/app/config.h.bkp 
+# Lee el primer argumento pasado al script
+config=$1
 
-sed -i 's,//#define USE_MRF24_TX,#define USE_MRF24_TX,g' libs/app/config.h
-sed -i 's,#define USE_MRF24_RX,//#define USE_MRF24_RX,g' libs/app/config.h
+# Realiza acciones en función del argumento
+case $config in
+    rx)
+        echo "Ejecutando con rx..."
+        # Agrega aquí las acciones específicas para config1
+        echo "Configuring as a receiver"
+        cp libs/app/config.22h libs/app/config.h.bkp 
 
-echo "Configure as Transmitter Tx ..."
+        sed -i 's,//#define USE_MRF24_TX,#define USE_MRF24_TX,g' libs/app/config.h
+        sed -i 's,#define USE_MRF24_RX,//#define USE_MRF24_RX,g' libs/app/config.h
+
+        echo "Configure as receiver Rx ..."
+        ;;
+    tx)
+        echo "Ejecutando con tx..."
+        echo "Configuring as a transmitter"
+        # Agrega aquí las acciones específicas para config2
+        sed -i 's,#define USE_MRF24_TX,//#define USE_MRF24_TX,g' libs/app/config.h
+        sed -i 's,//#define USE_MRF24_RX,#define USE_MRF24_RX,g' libs/app/config.h
+        echo "Configure as Transmitter Tx ..."
+        ;;
+    *)
+        echo "No hay cambios : $config"
+        exit 1
+        ;;
+esac
+
+# El resto del script aquí...
+
+# Ejemplo: Mostrar el contenido del archivo de configuración
+echo "Contenido de $config:"
+cat "$config"
