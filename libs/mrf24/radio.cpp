d11152824753a6f772ca1d361866327c3c58395f
bee <bee@macbook.local>
2023-12-01 01:27:12 -0300

    update 202312010127

--- libs/mrf24/radio.cpp
+++ libs/mrf24/radio.cpp
@@ -61,12 +61,14 @@ Radio_t::Radio_t()
     {
         gpio->app(flag);
         Run(flag);
-        flag = mrf24j40_spi.interrupt_handler();
+        mrf24j40_spi.interrupt_handler();
     }
 }
 
 void Radio_t::Run(bool& flag) {
+
     flag = mrf24j40_spi.check_flags(&handle_rx, &handle_tx);
+    
     const unsigned long current_time = 1000000;//1000000 original
     if (current_time - last_time > tx_interval) {
         last_time = current_time;
