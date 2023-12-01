e096b2ee4a465a9336e17139fc8b927032e9c9e7
bee <bee@macbook.local>
2023-12-01 00:59:11 -0300

    update 202312010059

--- libs/app/config.h
+++ libs/app/config.h
@@ -3,8 +3,8 @@
 //#define USE_MRF24_TX
 #define USE_MRF24_RX
 
-//#define USE_MAC_ADDRESS_LONG
-#define USE_MAC_ADDRESS_SHORT
+#define USE_MAC_ADDRESS_LONG
+//#define USE_MAC_ADDRESS_SHORT
     //Enable database 
 //#define ENABLE_DATABASE
 
