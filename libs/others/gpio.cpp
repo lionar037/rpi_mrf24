81bec72fedbe4bb413a3d8c1da0af48096c39d82
bee <bee@macbook.local>
2023-12-01 01:52:14 -0300

    update 202312010152

--- libs/others/gpio.cpp
+++ libs/others/gpio.cpp
@@ -108,9 +108,9 @@ if(flag){
     if(!fileGpio23){
         const bool result_input = std::system("echo 23 > /sys/class/gpio/export");
         if (result_input == 0) {
-            //#ifdef DBG_GPIO
+            #ifdef DBG_GPIO
                 std::cout << "Pin GPIO 23 exported successfully." << std::endl;
-            //#endif
+            #endif
         } else {
             #ifdef DBG_GPIO
                 std::cerr << "Error exporting GPIO 23." << std::endl;
