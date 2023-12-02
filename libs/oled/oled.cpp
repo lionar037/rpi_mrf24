3f82303118a435c2f6b3ab0b92cf5e7e606bdb13
bee <bee@macbook.local>
2023-12-02 03:46:06 -0300

    update 202312020346

--- libs/oled/oled.cpp
+++ libs/oled/oled.cpp
@@ -1,13 +1,15 @@
 #include <bcm2835.h>
 #include <time.h>
 #include <stdio.h>
-
+#ifdef USE_MRF24_RX
+    #include <SSD1306_OLED.hpp>
+#endif
 #include <oled/oled.h>
 
 
 namespace OLED{
 #ifdef USE_MRF24_RX
-#include <SSD1306_OLED.hpp>
+
 
 SSD1306 myOLED(myOLEDwidth ,myOLEDheight) ; // instantiate  an object 
 
