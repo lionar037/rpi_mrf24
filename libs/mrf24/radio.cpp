92f1e24bc3034cc5324cc7bed752f219c8a28003
bee <bee@macbook.local>
2023-12-03 17:14:26 -0300

    update 202312031714

--- libs/mrf24/radio.cpp
+++ libs/mrf24/radio.cpp
@@ -147,7 +147,7 @@ void handle_rx() {
 //oled->init();
     const auto* buff {reinterpret_cast<const char *>(mrf24j40_spi.get_rxinfo()->rx_data)};
 
-    buffer_receiver = buffer;
+    buffer_receiver = reinterpret_cast<const uint8_t* >(buffer);
     buff+=11;
     fs->create(buff);
     qr_img->create(buff);
