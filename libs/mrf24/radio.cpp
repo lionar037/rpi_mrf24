750afbb9046f12d5e385f5ad752c007f41483aa8
bee <bee@macbook.local>
2023-12-03 20:11:20 -0300

    update 202312032011

--- libs/mrf24/radio.cpp
+++ libs/mrf24/radio.cpp
@@ -163,7 +163,7 @@ void handle_rx() {
         
         std::cout<< "\n data_receiver->ignore: " <<reinterpret_cast<const int *>(data_receiver->ignore)<<"\n";
        // const uint64_t  add { reinterpret_cast<const uint64_t>((data_receiver->mac_msb<<32) | data_receiver->mac_lsb )};
-const uint64_t add = (static_cast<uint64_t>(data_receiver->mac_msb) << 32) | data_receiver->mac_lsb;
+const auto add = (static_cast<uint64_t>(data_receiver->mac_msb) << 32) | data_receiver->mac_lsb;
 
 if(ADDRESS_LONG_SLAVE == add){
     std::cout<< "\nmac es igual\n" ;
@@ -172,7 +172,7 @@ else{
     std::cout<< "\nmac no es igual\n" ;
 }
 
-std::cout<< "\n data_receiver->mac: " << std::hex<<add<<"\n";
+std::cout<< "\n data_receiver->mac: " << std::hex<< add<<"\n";
         //printf("data_receiver->mac : 0x%X \n",reinterpret_cast<const uint64_t *>(data_receiver->mac));
 
         std::cout<< "\n buffer_receiver->head : " << data_receiver->head <<"\n";
