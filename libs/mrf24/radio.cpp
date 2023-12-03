879c11be5dccd6920e71f7ed292853044bbc7eda
bee <bee@macbook.local>
2023-12-03 19:41:29 -0300

    update 202312031941

--- libs/mrf24/radio.cpp
+++ libs/mrf24/radio.cpp
@@ -172,7 +172,7 @@ else{
 
         //printf("data_receiver->mac : 0x%X \n",reinterpret_cast<const uint64_t *>(data_receiver->mac));
 
-        std::cout<< "\n buffer_receiver->head : " << data_receiver->head <<"\n";
+        std::cout<< "\n buffer_receiver->head : " << reinterpret_cast<const char *>(data_receiver->head) <<"\n";
         std::cout<< "buffer_receiver->size : " << reinterpret_cast<const int *>(data_receiver->size)<<"\n";
         std::cout<< "data_receiver->data : " <<reinterpret_cast<const char *>(data_receiver->data)<<"\n";
 
