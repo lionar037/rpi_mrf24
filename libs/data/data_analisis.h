4713704c287814a1b48ba4406d4affba5ec061d7
bee <bee@macbook.local>
2023-12-03 18:16:46 -0300

    update 202312031816

--- libs/data/data_analisis.h
+++ libs/data/data_analisis.h
@@ -13,7 +13,7 @@ namespace DATA{
     typedef struct Buffer
     {
     //MACADDRESS ignore;   
-        uint64_t ma{0x00};
+        uint64_t mac{0x00};
         uint8_t head{0x00};
         uint16_t size{0x00};
         uint8_t data[107]{0x00};
