a8364a9a33e3cd79eb7bd8f477b7b84ab8002b8c
bee <bee@macbook.local>
2023-12-03 18:11:32 -0300

    update 202312031811

--- libs/data/data_analisis.h
+++ libs/data/data_analisis.h
@@ -4,7 +4,7 @@ namespace DATA{
 
     typedef struct MacAdress
     {
-        uint8_t ignore[3];
+        //uint8_t ignore[3];
         uint64_t mac;
         //uint8_t ignore2;
     }MACADDRESS;
@@ -15,8 +15,7 @@ namespace DATA{
     MACADDRESS mac;   
         uint8_t head;
         uint16_t size;
-        uint8_t data[128];
-
+        uint8_t data[107];
 
     }BUFFER;
 
