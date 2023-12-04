f881fc09f91d150796a63eac218874d6bff86f8b
bee <bee@macbook.local>
2023-12-04 20:05:14 -0300

    update 202312042005

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ author : amaya
 
 //#include <run.h>
 #include <memory>
-#include <mrf24/radio.h>
+#include <mrf24/src/radio.h>
 
 int main(){
     auto mrf { std::make_unique<MRF24J40::Radio_t>() };
