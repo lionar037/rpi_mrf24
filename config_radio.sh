6dc871f3fa6d5264a466328cdd4af3ad8c06f71b
bee <bee@macbook.local>
2023-12-04 20:33:11 -0300

    update 202312042033

--- config_radio.sh
+++ config_radio.sh
@@ -15,15 +15,15 @@ case $config in
         # Agrega aquí las acciones específicas para config1
         echo "Configuring as a transmitter"
         #cp libs/app/src/config.h libs/app/src/config.h.bkp 
-        sed -i 's,//#define USE_MRF24_TX,#define USE_MRF24_TX,g' libs/app/src/config.h
-        sed -i 's,#define USE_MRF24_RX,//#define USE_MRF24_RX,g' libs/app/src/config.h
+        sed -i 's,//#define USE_MRF24_TX,#define USE_MRF24_TX,g' src/app/src/config.h
+        sed -i 's,#define USE_MRF24_RX,//#define USE_MRF24_RX,g' src/app/src/config.h
         echo "Configure as transmitter Rx ..."
         ;;
     rx)
         echo "Configuring as a receiver"
         # Agrega aquí las acciones específicas para config2
-        sed -i 's,#define USE_MRF24_TX,//#define USE_MRF24_TX,g' libs/app/src/config.h
-        sed -i 's,//#define USE_MRF24_RX,#define USE_MRF24_RX,g' libs/app/src/config.h
+        sed -i 's,#define USE_MRF24_TX,//#define USE_MRF24_TX,g' src/app/src/config.h
+        sed -i 's,//#define USE_MRF24_RX,#define USE_MRF24_RX,g' src/app/src/config.h
         echo "Configure as receiver Tx ..."
         ;;
     *)
