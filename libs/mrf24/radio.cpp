a46c3d0b132e191cce63713ec83535ac2854ca1b
bee <bee@macbook.local>
2023-12-04 02:35:38 -0300

    update 202312040235

--- libs/mrf24/radio.cpp
+++ libs/mrf24/radio.cpp
@@ -118,13 +118,12 @@ void Radio_t::Run(bool& flag) {
         
         
         #ifdef MACADDR64
-        
-            
             mrf24j40_spi.send(ADDRESS_LONG_SLAVE, msj);
            // mrf24j40_spi.send64(ADDRESS_LONG_SLAVE, buffer_transmiter);
            //mrf24j40_spi.send(ADDRESS_LONG_SLAVE, msj);
         #elif defined(MACADDR16)
-        mrf24j40_spi.send16(ADDR_SLAVE, pf );
+            mrf24j40_spi.send(ADDR_SLAVE, msj);
+            //mrf24j40_spi.send(ADDR_SLAVE, pf );
             //mrf24j40_spi.send16(ADDR_SLAVE, MSJ );//send data//original//mrf24j40_spi.send16(0x4202, "abcd")
         #endif
     #endif
