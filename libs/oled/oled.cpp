9622be8771ffaab9fc5272cd19e6d0409958b003
bee <bee@macbook.local>
2023-12-04 02:58:57 -0300

    update 202312040258

--- libs/oled/oled.cpp
+++ libs/oled/oled.cpp
@@ -25,10 +25,10 @@ SSD1306 myOLED(myOLEDwidth ,myOLEDheight) ; // instantiate  an object
             	myOLED.setTextSize(1);
                 	//myOLED.setFontNum(OLEDFontType_Thick);
                         //myOLED.setFontNum(OLEDFontType_SevenSeg);
-                        //myOLED.setFontNum(OLEDFontType_Wide);
+                        myOLED.setFontNum(OLEDFontType_Wide);
                         //myOLED.setFontNum(OLEDFontType_Tiny);
                         //myOLED.setFontNum(OLEDFontType_Homespun);
-                        myOLED.setFontNum(OLEDFontType_Default);
+                        //myOLED.setFontNum(OLEDFontType_Default);
                 	//myOLED.OLEDInvert(1); // Inverted
             myOLED.setTextColor(WHITE);
             myOLED.setCursor(0, 0);
