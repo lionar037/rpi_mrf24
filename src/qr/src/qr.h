f89b084f8bfb61ff400eca938ffe42d83f971a70
bee <bee@macbook.local>
2023-12-05 01:35:12 -0300

    update 202312050135

--- src/qr/src/qr.h
+++ src/qr/src/qr.h
@@ -13,7 +13,7 @@ namespace TYME{
 
 namespace QR{
 
-using QrCode_t = struct DATA::qr_oled;
+using QrCode_t =  DATA::qr_oled;
 
     struct Qr_t : public WORK::Work_t
     {
@@ -21,12 +21,12 @@ using QrCode_t = struct DATA::qr_oled;
             ~Qr_t()=default;
             bool                create                  (const char* );
             //template <typename T>
-            const QrCode_t* create_qr (const char*,  std::vector<unsigned char>&) ;
+            const struct QrCode_t* create_qr (const char*,  std::vector<unsigned char>&) ;
             
             //unsigned char*      get_buffer_pointer      (std::vector<unsigned char>&); 
         private:
             std::vector<unsigned char>vs{};
-            QrCode_t QrOled{};
+            struct QrCode_t QrOled{};
     };
 
 
