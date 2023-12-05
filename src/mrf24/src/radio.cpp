28579a6e459c3c52142ab5062f6c1893e7dd7f57
bee <bee@macbook.local>
2023-12-05 17:39:11 -0300

    update 202312051739

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -178,7 +178,7 @@ void handle_rx() {
     //const auto packet_data_tmp {reinterpret_cast<const DATA::PACKET_RX*>(packet_data)};
   
   //std::string_view* packet_data (reinterpret_cast<const unsigned char*>(mrf24j40_spi.get_rxinfo()->rx_data));
-  std::cout<<"packet_data->data() : ";
+  std::cout<<"packet_data->data() : \n";
   
    std::cout<<reinterpret_cast<const unsigned char*>(packet_data->data())<<"\n";
 
