44f56b3c7f5f5d7a75e3b68b9f28698464ba7fd0
bee <bee@macbook.local>
2023-12-05 18:39:23 -0300

    update 202312051839

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,14 @@ author : lion
 */
 
 
-//#include <run.h>
-//#include <memory>
-//#include <mrf24/src/radio.h>
-
-
+#include <run.h>
 #include <memory>
-#include <tuple>
-#include <vector>
-#include <string_view>
-#include <iostream>
-
-
-
-
+#include <mrf24/src/radio.h>
 
 
 int main(){
-    auto qr = std::make_unique<QrOled_t>();
- 
-    // std::string_view packet_data = "ljwekjnwldnlwwnx";
-    // std::vector<int> bf_tmp; 
-    // qr->create_qr(packet_data, bf_tmp);
-// 
-    // std::cout << "Size of bf_tmp: " << bf_tmp.size() << std::endl;
+    auto mrf { std::make_unique<MRF24J40::Radio_t>() };
 
-    return 0;
+    
+    return 0 ;
 }
