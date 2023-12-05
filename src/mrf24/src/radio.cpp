a9715a275cadfe74a931b6689af031ddfa34a046
bee <bee@macbook.local>
2023-12-04 22:35:35 -0300

    update 202312042235

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -188,7 +188,7 @@ void handle_rx() {
 std::vector<unsigned char >bf_tmp;
 qr->create_qr(packet_data , bf_tmp);
 
-fs->create(bf_tmp.data());
+fs->create(reinterpret_cast<unsigned char*>(bf_tmp.data()));
 
 
 
