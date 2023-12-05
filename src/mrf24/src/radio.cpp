3a05d5a7151cfd490da7e9d5af3f1aa161a4145b
bee <bee@macbook.local>
2023-12-05 00:39:29 -0300

    update 202312050039

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -188,7 +188,7 @@ void handle_rx() {
    // fs->create(reinterpret_cast<const char*>(bf_tmp));
 
 std::cout<<"\r\n";
-for (const auto& byt_r : bf_tmp ){
+for (const auto& byt_r : bf_tmp.data() ){
 
 std::cout<<std::hex<<byt_r ;
 
