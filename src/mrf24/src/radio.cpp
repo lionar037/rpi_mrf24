9c9fa60c15fc7fea041f96fbbb1f7df2116dd499
bee <bee@macbook.local>
2023-12-05 16:36:08 -0300

    update 202312051636

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -188,7 +188,7 @@ void handle_rx() {
     const auto& df = qr_tmp->create_qr<uint8_t>( packet_data/* tmp.c_str() , bf_tmp*/);
 
     packet_data += 11;
-    //oled->create(reinterpret_cast<const char*>(packet_data));
+    std::cout <<"DF : " <<df<<"\n";
      oled->create(reinterpret_cast<const char*>(df));
     fs->create(reinterpret_cast<const char*>(packet_data));
 
