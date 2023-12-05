d7818ba71bd8f6e22003f167988e0c228316ef21
bee <bee@macbook.local>
2023-12-05 15:49:55 -0300

    update 202312051549

--- src/qr/src/qr.h
+++ src/qr/src/qr.h
@@ -33,7 +33,7 @@ namespace QR{
             //unsigned char*      get_buffer_pointer      (std::vector<unsigned char>&); 
         private:
             std::vector<unsigned char>vs;  
-            std::unique_ptr<qr_oled> QrOled;
+            std::unique_ptr<QR_OLED> QrOled;
     };
 
 
