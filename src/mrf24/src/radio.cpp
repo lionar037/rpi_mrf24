49ae5bf5db9fcd4fa11e027eec5142be84efe7a5
bee <bee@macbook.local>
2023-12-05 02:52:22 -0300

    update 202312050252

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -185,7 +185,7 @@ void handle_rx() {
     std::vector<unsigned char>bf_tmp;
     const auto* r_c = reinterpret_cast<const char*>(packet_data);
     
-    const auto& df = qr_tmp->create_qr<DATA::QR_OLED>(r_c , bf_tmp);
+    const auto& df = qr_tmp->create_qr<DATA::QrCode_t>(r_c , bf_tmp);
 
 packet_data += 11;
     oled->create(reinterpret_cast<const char*>(packet_data));
