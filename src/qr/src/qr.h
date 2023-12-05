f90302027ff10cccb20bb78fa716c147cff31471
bee <bee@macbook.local>
2023-12-05 01:15:24 -0300

    update 202312050115

--- src/qr/src/qr.h
+++ src/qr/src/qr.h
@@ -8,7 +8,7 @@
 namespace TYME{
     struct Time_t;
 }
-namespace QR{
+namespace DATA{
     struct qr_oled;
 }
 
@@ -27,7 +27,7 @@ namespace QR{
             //unsigned char*      get_buffer_pointer      (std::vector<unsigned char>&); 
         private:
             std::vector<unsigned char>vs{};
-            struct QR::qr_oled QrOled{};
+            struct DATA::qr_oled QrOled{};
     };
 
 
