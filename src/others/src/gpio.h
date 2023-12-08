5e180fad4b6d12482dda3ec050cfe81187a89ded
bee <bee@macbook.local>
2023-12-08 13:56:44 -0300

    update 202312081356

--- src/others/src/gpio.h
+++ src/others/src/gpio.h
@@ -2,7 +2,7 @@
 
 #define IN_INTERRUPT    23
 #define OUT_INTERRUPT   12    //GPIO LED
-#define READING_STEPS   1     //10 originalmente
+#define READING_STEPS   2     //10 originalmente
 
 #define SYSFS_GPIO_PATH             "/sys/class/gpio"
 #define SYSFS_GPIO_EXPORT_FN        "/export"
