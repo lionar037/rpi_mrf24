b03a3469a9e41f7e3ae52c57ae893bbc61b09469
bee <bee@macbook.local>
2023-12-09 00:19:18 -0300

    update 202312090019

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(){
 //const int ThreadCant{3};
 
 
-    try    {
+    //try    {
 
         // for (int i = 0; i < ThreadCant; ++i) {
             // threadVect.emplace_back([&mrf]() {
@@ -26,9 +26,9 @@ int main(){
         // for (auto& threadRun : threadVect) {
             // threadRun.join();
         // }      
-     }
-     catch(...){
-         std::cerr<<"\nerror :(\n";
-     }
+    //  }
+    //  catch(...){
+        //  std::cerr<<"\nerror :(\n";
+    //  }
     return 0 ;
 }
