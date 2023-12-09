6006ffd12e715613ee6c4eba8b6b43d33004beb3
bee <bee@macbook.local>
2023-12-08 23:39:21 -0300

    update 202312082339

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,23 @@ author : lion
 
 int main(){
     auto mrf { std::make_unique<MRF24J40::Radio_t>() };
-    std::vector<std::thread> Thread;
-const int ThreadCant{3};
+    std::vector<std::thread> threadVect;
+const std::size_t ThreadCant{3};
 
 
     try
     {
 
-        for (int i = 0; i < ThreadCant; ++i) {
-            Thread.emplace_back([&mrf]() {
+        for (std::size_t i = 0; i < ThreadCant; ++i) {
+            threadVect.emplace_back([&mrf]() {
            mrf->Run();
             });
         }
 
         // Esperar a que todos los hilos terminen
-        for (auto& hilo : Thread) {
-            hilo.join();
-        }
-
-
-
-      
+        for (auto& threadRun : threadVect) {
+            threadRun.join();
+        }      
     }
     catch(...){
         std::cerr<<"\nerror :(\n";
