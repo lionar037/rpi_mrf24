23f19f20b18cd629ab578e53a674c3dffae161a3
bee <bee@macbook.local>
2023-12-09 00:16:41 -0300

    update 202312090016

--- src/oled/src/oled.cpp
+++ src/oled/src/oled.cpp
@@ -18,7 +18,7 @@ SSD1306 myOLED(myOLEDwidth ,myOLEDheight) ; // instantiate  an object
         {
         static bool flag = true;
         static int count { 0 };
-        static std::string textOledTmp="@ rnjferfnelrfkelrkferlfmkerlf";
+        static std::string textOledTmp="@";
 if(std::strcmp(textOledTmp.c_str(),textOled.data())){flag=false;}
 else{flag=true;}
 
