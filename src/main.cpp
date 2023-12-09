c0c633a7a68d8698c77466f174efa48386de9444
bee <bee@macbook.local>
2023-12-08 23:30:24 -0300

    update 202312082330

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,33 @@ author : lion
 #include <iostream>
 #include <memory>
 #include <thread>
+#include <vector>
 #include <mrf24/src/radio.h>
 
 
 int main(){
     auto mrf { std::make_unique<MRF24J40::Radio_t>() };
+    std::vector<std::thread> hilos;
+
+
 
     try
     {
-        mrf->Run();
+
+        for (int i = 0; i < numHilos; ++i) {
+            hilos.emplace_back([&mrf]() {
+           mrf->Run();
+            });
+        }
+
+        // Esperar a que todos los hilos terminen
+        for (auto& hilo : hilos) {
+            hilo.join();
+        }
+
+
+
+      
     }
     catch(...){
         std::cerr<<"\nerror :(\n";
