54a4075263eb1892d3bf6b4074a12011dbf4c0d1
bee <bee@macbook.local>
2023-12-09 01:36:17 -0300

    update 202312090136

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -196,7 +196,7 @@ void handle_rx() {
     for (auto& byte : mrf24j40_spi.get_rxinfo()->rx_data)std::cout<<byte;
     std::cout<<"\n";
 
-    update(mrf24j40_spi.get_rxinfo()->rx_data);
+    update(reinterpret_cast<const char*>(mrf24j40_spi.get_rxinfo()->rx_data));
 
     #ifdef DBG_PRINT_GET_INFO 
       
