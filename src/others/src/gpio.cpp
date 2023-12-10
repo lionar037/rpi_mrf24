aa6fee1eb8db4d713fd570c7ff4df9d53d7f185b
bee <bee@macbook.local>
2023-12-10 01:12:48 -0300

    update 202312100112

--- src/others/src/gpio.cpp
+++ src/others/src/gpio.cpp
@@ -154,27 +154,34 @@ namespace GPIO{
         int looper = 0;
         char *buf[64];
 
-        // settings( gpio_in  , DIR_IN  ,fileGpioInput);
+         settings( gpio_in  , DIR_IN  ,fileGpioInput);
         // settings( gpio_out , DIR_OUT ,fileGpioOutput);
 
 
         
-       const std::string filePathGpio23 = "/sys/class/gpio/gpio23/direction";
-        std::ifstream fileGpio23(filePathGpio23);
-        if(!fileGpio23){
-            const bool result_input = std::system("echo 23 > /sys/class/gpio/export");
-            if (result_input == 0) {
-                #ifdef DBG_GPIO
-                    std::cout << "Pin GPIO 23 exported successfully." << std::endl;
-                #endif
-            } else {
-                #ifdef DBG_GPIO
-                    std::cerr << "Error exporting GPIO 23." << std::endl;
-                #endif
-                return false;
-            }
-        }
-  
+    //    const std::string filePathGpio23 = "/sys/class/gpio/gpio23/direction";
+        // std::ifstream fileGpio23(filePathGpio23);
+        // if(!fileGpio23){
+            // const bool result_input = std::system("echo 23 > /sys/class/gpio/export");
+            // if (result_input == 0) {
+                // #ifdef DBG_GPIO
+                    // std::cout << "Pin GPIO 23 exported successfully." << std::endl;
+                // #endif
+            // } else {
+                // #ifdef DBG_GPIO
+                    // std::cerr << "Error exporting GPIO 23." << std::endl;
+                // #endif
+                // return false;
+            // }
+        // }
+            // gpio_unexport(gpio_in);
+            // gpio_export(gpio_in);
+            // gpio_set_direction(gpio_in,DIR_IN);            
+             gpio_set_edge(gpio_in,EDGE_FALLING);
+
+
+
+
        const std::string filePathGpio12 = "/sys/class/gpio/gpio12/direction";
         std::ifstream fileGpio12(filePathGpio12);
         if(!fileGpio12){
@@ -191,14 +198,11 @@ namespace GPIO{
             }
         }         
         gpio_unexport(gpio_out);
-        gpio_unexport(gpio_in);
         gpio_export(gpio_out);
-        gpio_export(gpio_in);
         gpio_set_direction(gpio_out,DIR_OUT);
-        gpio_set_direction(gpio_in,DIR_IN);
 
         gpio_set_value(gpio_out,VALUE_HIGH);
-        gpio_set_edge(gpio_in,EDGE_FALLING);
+
           
         int gpio_in_fd = gpio_get_fd_to_value(gpio_in);
         m_gpio_in_fd = gpio_in_fd;
