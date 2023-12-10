a2e838ca0d91af96f051d95c75ca412bb79deb08
bee <bee@macbook.local>
2023-12-10 14:49:17 -0300

    update 202312101449

--- src/qr/src/qr.cpp
+++ src/qr/src/qr.cpp
@@ -11,15 +11,16 @@ namespace QR{
     bool Qr_t::create(const std::string_view& fname ) {
     auto            monitor     {std::make_unique <FFLUSH::Fflush_t>()};
         RST_COLOR() ;
-        SET_COLOR(SET_COLOR_WHITE_TEXT);
+
         // Configuración del código QR
         //QRcode* qr = QRcode_encodeString(fname.data(), 0, QR_ECLEVEL_L, QR_MODE_8, 1);
 
         auto qr = std::unique_ptr<QRcode>(QRcode_encodeString(fname.data(), 0, QR_ECLEVEL_L, QR_MODE_8, 1));
         // Imprime el código QR en la consola
         std::cout << "\033[2J\033[H" << std::flush;
-        monitor->print(" ",0,0);
-        std::cout << "\n";
+        monitor->print("\n",0,0);
+                SET_COLOR(SET_COLOR_WHITE_TEXT);
+        //std::cout << "\n";
         for (int y = 0; y < qr->width; y++) {
             for (int x = 0; x < qr->width; x++) {
                 std::cout << (qr->data[y * qr->width + x] & 1 ? "██" : "  ");
