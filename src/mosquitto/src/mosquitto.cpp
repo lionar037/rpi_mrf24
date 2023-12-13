b08f2de60d861aa560d2764ba25d8d0ca7555064
bee <bee@macbook.local>
2023-12-13 17:16:39 -0300

    update 202312131716

--- src/mosquitto/src/mosquitto.cpp
+++ src/mosquitto/src/mosquitto.cpp
@@ -11,17 +11,13 @@
 
 namespace MOSQUITTO{
 
-
-
-
     void on_connect(struct mosquitto *mosq, void *obj, int rc) {
         printf("ID: %d\n", * (int *) obj);
         if(rc) {
             printf("Error with result code: %d\n", rc);
             exit(-1);
         }
-        mosquitto_subscribe(mosq, NULL, "home/room", 0);
-        
+        mosquitto_subscribe(mosq, NULL, "home/room", 0);        
     }
 
     void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg) {
@@ -48,28 +44,28 @@ namespace MOSQUITTO{
     }
 
 
-int Mosquitto_t::sub() {
-	int rc, id=12;
-	mosquitto_lib_init();
-	struct mosquitto *mosq;
-	mosq = mosquitto_new("subscribe-test", true, &id);
-	mosquitto_connect_callback_set(mosq, on_connect);
-	mosquitto_message_callback_set(mosq, on_message);	
-	rc = mosquitto_connect(mosq, "HOSTNAME_MOSQUITTO", 1883, 10);
-    mosquitto_username_pw_set(mosq, "pi", "zero");
-	if(rc) {
-		printf("Could not connect to Broker with return code %d\n", rc);
-		return -1;
-	}
-	mosquitto_loop_start(mosq);
-	printf("Press Enter to quit...\n");
-	getchar();
-	mosquitto_loop_stop(mosq, true);
-	mosquitto_disconnect(mosq);
-	mosquitto_destroy(mosq);
-	mosquitto_lib_cleanup();
-	return 0;
-}
+    int Mosquitto_t::sub() {
+    	int rc, id=12;
+    	mosquitto_lib_init();
+    	struct mosquitto *mosq;
+    	mosq = mosquitto_new("subscribe-test", true, &id);
+    	mosquitto_connect_callback_set(mosq, on_connect);
+    	mosquitto_message_callback_set(mosq, on_message);	
+    	rc = mosquitto_connect(mosq, "HOSTNAME_MOSQUITTO", 1883, 10);
+        mosquitto_username_pw_set(mosq, "pi", "zero");
+    	if(rc) {
+    		printf("Could not connect to Broker with return code %d\n", rc);
+    		return -1;
+    	}
+    	mosquitto_loop_start(mosq);
+    	printf("Press Enter to quit...\n");
+    	getchar();
+    	mosquitto_loop_stop(mosq, true);
+    	mosquitto_disconnect(mosq);
+    	mosquitto_destroy(mosq);
+    	mosquitto_lib_cleanup();
+    	return 0;
+    }
 
     Mosquitto_t::Mosquitto_t()
     //:mosco {std::make_unique<mosquitto>()}
@@ -78,13 +74,5 @@ int Mosquitto_t::sub() {
         //pub();
         //sub();
     }
-
-
-
-
-
-
-
-
 
 }
