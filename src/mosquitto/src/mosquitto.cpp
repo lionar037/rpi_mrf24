86b0caac73a011ca1b2d91c8a5fe9d0c11a8c839
bee <bee@macbook.local>
2023-12-13 01:41:14 -0300

    update 202312130141

--- src/mosquitto/src/mosquitto.cpp
+++ src/mosquitto/src/mosquitto.cpp
@@ -43,7 +43,7 @@ namespace MOSQUITTO{
     Mosquitto_t::Mosquitto_t()
     //:mosco {std::make_unique<mosquitto>()}
     {
-//mosco = mosquitto_new("publisher-test", true, NULL);
+        //mosco = mosquitto_new("publisher-test", true, NULL);
         init();
     }
 
@@ -52,48 +52,41 @@ namespace MOSQUITTO{
 
 void Mosquitto_t::sub() {
 	int rc, id=12;
-
 	mosquitto_lib_init();
-
 	struct mosquitto *mosq;
-
 	mosq = mosquitto_new("subscribe-test", true, &id);
 	mosquitto_connect_callback_set(mosq, on_connect);
-	mosquitto_message_callback_set(mosq, on_message);
-	
+	mosquitto_message_callback_set(mosq, on_message);	
 	rc = mosquitto_connect(mosq, "raspberry.local", 1883, 10);
 	if(rc) {
 		printf("Could not connect to Broker with return code %d\n", rc);
 		return -1;
 	}
-
 	mosquitto_loop_start(mosq);
 	printf("Press Enter to quit...\n");
 	getchar();
 	mosquitto_loop_stop(mosq, true);
-
 	mosquitto_disconnect(mosq);
 	mosquitto_destroy(mosq);
 	mosquitto_lib_cleanup();
-
 	return 0;
 }
 
 
 
 
-void Mosquitto_t::on_connect(struct mosquitto *mosq, void *obj, int rc) {
-	printf("ID: %d\n", * (int *) obj);
-	if(rc) {
-		printf("Error with result code: %d\n", rc);
-		exit(-1);
-	}
-	mosquitto_subscribe(mosq, NULL, "test/t1", 0);
-}
+    void Mosquitto_t::on_connect(struct mosquitto *mosq, void *obj, int rc) {
+        printf("ID: %d\n", * (int *) obj);
+        if(rc) {
+            printf("Error with result code: %d\n", rc);
+            exit(-1);
+        }
+        mosquitto_subscribe(mosq, NULL, "home/room", 0);
+    }
 
-void Mosquitto_t::on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg) {
-	printf("New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
-}
+    void Mosquitto_t::on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg) {
+        printf("New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
+    }
 
 
 }
