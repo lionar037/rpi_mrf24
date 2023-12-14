1f1f5c03d1cae46dba2a34c93e05df312cc056c1
bee <bee@macbook.local>
2023-12-13 22:10:33 -0300

    update 202312132210

--- src/mrf24/src/radio.h
+++ src/mrf24/src/radio.h
@@ -82,7 +82,7 @@ namespace MRF24J40{
         std::unique_ptr<GPIO::Gpio> gpio{};    
 
         //static std::unique_ptr<MOSQUITTO::Mosquitto_t>mosq{};    
-        static std::unique_ptr<uint32_t>mosq{};    
+        static std::unique_ptr<uint32_t>mosq;    
         
     };
 
