8f293d3c28115204028cb6b43d938a6360444dd8
bee <bee@macbook.local>
2023-12-13 21:30:10 -0300

    update 202312132130

--- src/mrf24/src/static.cpp
+++ src/mrf24/src/static.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <memory>
-#include <thread>
-#include <vector>
-extern "C"{
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <mosquitto.h>
-}
+// #include <iostream>
+// #include <memory>
+// #include <thread>
+// #include <vector>
+// extern "C"{
+//     #include <stdio.h>
+//     #include <stdlib.h>
+//     #include <mosquitto.h>
+// }
 
 
-struct Mosquitto_t{
+// struct Mosquitto_t{
 
-Mosquitto_t();
-~Mosquitto_t()=default;
-void funcion(void (*)());
-static void rx();
+// Mosquitto_t();
+// ~Mosquitto_t()=default;
+// void funcion(void (*)());
+// static void rx();
 
-private:
-    std::unique_ptr<mosquitto>mosq;
-};
+// private:
+//     std::unique_ptr<mosquitto>mosq;
+// };
 
-void Mosquitto_t::rx(){
+// void Mosquitto_t::rx(){
 
-}
-
-
-Mosquitto_t::Mosquitto_t()
-:   mosq{std::make_unique<mosquitto>()}
-{
-
-}
+// }
 
 
-void funcion(void (*rx)()){
+// Mosquitto_t::Mosquitto_t()
+// :   mosq{std::make_unique<mosquitto>()}
+// {
 
-}
+// }
 
-int main(){
-std::unique_ptr<Mosquitto_t> mo{std::make_unique<Mosquitto_t> ()}
 
-    return 0;
-}
+// void funcion(void (*rx)()){
+
+// }
+
+// int main(){
+// std::unique_ptr<Mosquitto_t> mo{std::make_unique<Mosquitto_t> ()}
+
+//     return 0;
+// }
