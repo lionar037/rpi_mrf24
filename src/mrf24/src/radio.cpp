83ec9bbedd8f5e7e4475b142c63417828d5bbcac
bee <bee@macbook.local>
2023-12-14 10:31:33 -0300

    update 202312141031

--- src/mrf24/src/radio.cpp
+++ src/mrf24/src/radio.cpp
@@ -261,11 +261,13 @@ monitor->print("RSSI : " + std::to_string(mrf24j40_spi.get_rxinfo()->rssi) ,file
 RST_COLOR() ;   
 SET_COLOR(SET_COLOR_RED_TEXT);
 const int temperature = mosq->pub();
-const std::string tempString=  reinterpret_cast<const char*>(mrf24j40_spi.get_rxinfo()->rx_data)   + "{ temp :" + std::to_string(temperature)+ " }";
+
+const std::string tempString= reinterpret_cast<std::string>(mrf24j40_spi.get_rxinfo()->rx_data)    + "{ temp :" + std::to_string(temperature)+ " }";
+//reinterpret_cast<const char*>(mrf24j40_spi.get_rxinfo()->rx_data) 
 update(tempString.data());
 SET_COLOR(SET_COLOR_YELLOW_TEXT);
 
-//mosq->sub();
+mosq->sub();
 }
 
     Radio_t::~Radio_t() {
