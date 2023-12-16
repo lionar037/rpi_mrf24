eec002f8b7b12d0483589464634d555cbc9cc022
bee <bee@macbook.local>
2023-12-15 21:06:13 -0300

    update 202312152106

--- src/others/src/tyme.cpp
+++ src/others/src/tyme.cpp
@@ -59,105 +59,100 @@ namespace TYME{
 
 
 
+    bool Time_t::getHourNTP(const std::string_view& servidorNTP, std::chrono::system_clock::time_point& resultado) {
+        const int puertoNTP = 123;
+        const int tamanoPaqueteNTP = 48;
 
+        // Obtener información sobre el servidor NTP
+        addrinfo hints, *res;
+        std::memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = IPPROTO_UDP;
 
-//#include <string_view>
+        if (getaddrinfo(servidorNTP.data(), std::to_string(puertoNTP).c_str(), &hints, &res) != 0) {
+            perror("Error al obtener información del servidor NTP");
+            return false;
+        }
 
-bool Time_t::obtenerHoraNTP(const std::string_view& servidorNTP, std::chrono::system_clock::time_point& resultado) {
-    const int puertoNTP = 123;
-    const int tamanoPaqueteNTP = 48;
+        int socketNTP = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (socketNTP == -1) {
+            perror("Error al crear el socket");
+            freeaddrinfo(res);
+            return false;
+        }
 
-    // Obtener información sobre el servidor NTP
-    addrinfo hints, *res;
-    std::memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
+        char paqueteNTP[tamanoPaqueteNTP];
+        std::memset(paqueteNTP, 0, sizeof(paqueteNTP));
+        paqueteNTP[0] = 0x1B;
 
-    if (getaddrinfo(servidorNTP.data(), std::to_string(puertoNTP).c_str(), &hints, &res) != 0) {
-        perror("Error al obtener información del servidor NTP");
-        return false;
-    }
+        if (sendto(socketNTP, paqueteNTP, sizeof(paqueteNTP), 0, res->ai_addr, res->ai_addrlen) == -1) {
+            perror("Error al enviar la solicitud al servidor NTP");
+            close(socketNTP);
+            freeaddrinfo(res);
+            return false;
+        }
 
-    int socketNTP = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (socketNTP == -1) {
-        perror("Error al crear el socket");
-        freeaddrinfo(res);
-        return false;
-    }
+        char respuestaPaquete[tamanoPaqueteNTP];
+        if (recv(socketNTP, respuestaPaquete, sizeof(respuestaPaquete), 0) == -1) {
+            perror("Error al recibir la respuesta del servidor NTP");
+            close(socketNTP);
+            freeaddrinfo(res);
+            return false;
+        }
 
-    char paqueteNTP[tamanoPaqueteNTP];
-    std::memset(paqueteNTP, 0, sizeof(paqueteNTP));
-    paqueteNTP[0] = 0x1B;
-
-    if (sendto(socketNTP, paqueteNTP, sizeof(paqueteNTP), 0, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("Error al enviar la solicitud al servidor NTP");
         close(socketNTP);
         freeaddrinfo(res);
-        return false;
-    }
 
-    char respuestaPaquete[tamanoPaqueteNTP];
-    if (recv(socketNTP, respuestaPaquete, sizeof(respuestaPaquete), 0) == -1) {
-        perror("Error al recibir la respuesta del servidor NTP");
-        close(socketNTP);
-        freeaddrinfo(res);
-        return false;
-    }
+        uint64_t segundos = (static_cast<uint8_t>(respuestaPaquete[40]) << 24) |
+                            (static_cast<uint8_t>(respuestaPaquete[41]) << 16) |
+                            (static_cast<uint8_t>(respuestaPaquete[42]) << 8) |
+                            static_cast<uint8_t>(respuestaPaquete[43]);
 
-    close(socketNTP);
-    freeaddrinfo(res);
+        uint64_t fracciones = (static_cast<uint8_t>(respuestaPaquete[44]) << 24) |
+                               (static_cast<uint8_t>(respuestaPaquete[45]) << 16) |
+                               (static_cast<uint8_t>(respuestaPaquete[46]) << 8) |
+                               static_cast<uint8_t>(respuestaPaquete[47]);
 
-    uint64_t segundos = (static_cast<uint8_t>(respuestaPaquete[40]) << 24) |
-                        (static_cast<uint8_t>(respuestaPaquete[41]) << 16) |
-                        (static_cast<uint8_t>(respuestaPaquete[42]) << 8) |
-                        static_cast<uint8_t>(respuestaPaquete[43]);
+        if (fracciones == 0) {
+            std::cerr << "Error: Fracciones de tiempo NTP iguales a cero." << std::endl;
+            return false;
+        }
 
-    uint64_t fracciones = (static_cast<uint8_t>(respuestaPaquete[44]) << 24) |
-                           (static_cast<uint8_t>(respuestaPaquete[45]) << 16) |
-                           (static_cast<uint8_t>(respuestaPaquete[46]) << 8) |
-                           static_cast<uint8_t>(respuestaPaquete[47]);
+        auto tiempoNTP = std::chrono::system_clock::time_point(std::chrono::seconds(segundos) + std::chrono::microseconds(fracciones * 1000000ULL / (1ULL << 32)));
 
-    if (fracciones == 0) {
-        std::cerr << "Error: Fracciones de tiempo NTP iguales a cero." << std::endl;
-        return false;
-    }
-
-    auto tiempoNTP = std::chrono::system_clock::time_point(std::chrono::seconds(segundos) + std::chrono::microseconds(fracciones * 1000000ULL / (1ULL << 32)));
-
-    // Ajustar a la zona horaria de Argentina (GMT-3)
-    auto ajusteZonaHoraria = std::chrono::hours(-3);
-    resultado = tiempoNTP + ajusteZonaHoraria;
-
-    return true;
-}
-
-int Time_t::timeUpdate() {
-    const std::string servidorNTP = "pool.ntp.org";
-
-    // Obtener la hora actual local
-    auto tiempoLocal = std::chrono::system_clock::now();
-
-    std::chrono::system_clock::time_point tiempoNTP;
-    if (obtenerHoraNTP(servidorNTP, tiempoNTP)) {
         // Ajustar a la zona horaria de Argentina (GMT-3)
         auto ajusteZonaHoraria = std::chrono::hours(-3);
-        //tiempoNTP += ajusteZonaHoraria;
-	//tiempoActual += ajusteZonaHoraria;
+        resultado = tiempoNTP + ajusteZonaHoraria;
 
-tiempoLocal+=ajusteZonaHoraria;
-
-	std::time_t tiempoActual = std::chrono::system_clock::to_time_t(tiempoLocal);
-        std::cout << "La hora actual en Argentina (obtenida de " << servidorNTP << ") es: "
-                  << std::put_time(std::localtime(&tiempoActual), "%c") << std::endl;
-
-        std::time_t tiempoNTP_t = std::chrono::system_clock::to_time_t(tiempoNTP);
-        std::cout << "La hora obtenida del servidor NTP es: "
-                  << std::put_time(std::localtime(&tiempoNTP_t), "%c") << std::endl;
-    } else {
-        std::cerr << "No se pudo obtener la hora del servidor NTP." << std::endl;
+        return true;
     }
 
-    return 0;
-}
+    int Time_t::timeUpdate() {
+        const std::string servidorNTP = "pool.ntp.org";
+
+        // Obtener la hora actual local
+        auto tiempoLocal = std::chrono::system_clock::now();
+
+        std::chrono::system_clock::time_point tiempoNTP;
+        if (getHourNTP(servidorNTP, tiempoNTP)) {
+            // Ajustar a la zona horaria de Argentina (GMT-3)
+            auto ajusteZonaHoraria = std::chrono::hours(-3);
+            //tiempoNTP += ajusteZonaHoraria;    	
+
+            tiempoLocal+=ajusteZonaHoraria;
+
+    	    std::time_t tiempoActual = std::chrono::system_clock::to_time_t(tiempoLocal);
+            std::cout << "La hora actual en Argentina (obtenida de " << servidorNTP << ") es: "
+                      << std::put_time(std::localtime(&tiempoActual), "%c") << std::endl;
+
+            std::time_t tiempoNTP_t = std::chrono::system_clock::to_time_t(tiempoNTP);
+            std::cout << "La hora obtenida del servidor NTP es: "
+                      << std::put_time(std::localtime(&tiempoNTP_t), "%c") << std::endl;
+        } else {
+            std::cerr << "No se pudo obtener la hora del servidor NTP." << std::endl;
+        }
+
+        return 0;
+    }
 }
