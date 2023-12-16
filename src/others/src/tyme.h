73425397d33444148c3bdb7b066b41b11073e964
bee <bee@macbook.local>
2023-12-15 21:07:28 -0300

    update 202312152107

--- src/others/src/tyme.h
+++ src/others/src/tyme.h
@@ -12,7 +12,7 @@ struct Time_t
     ~Time_t();
     void delay_ms(const uint32_t);
     const std::string get_tyme();
-    bool obtenerHoraNTP(const std::string_view& , std::chrono::system_clock::time_point& ) ;
+    bool getHourNTP(const std::string_view& , std::chrono::system_clock::time_point& ) ;
     int timeUpdate() ;
     private:
         uint32_t m_delay{0};
