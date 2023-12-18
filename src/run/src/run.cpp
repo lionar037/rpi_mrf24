3a4321576c93e5385322fe0510cae8458ee6d59a
bee <bee@macbook.local>
2023-12-17 22:59:47 -0300

    update 202312172259

--- src/run/src/run.cpp
+++ src/run/src/run.cpp
@@ -10,8 +10,8 @@ namespace RUN{
 
 void Run_t::start(){
         system("clear"); 
-    auto mrf { std::make_unique<MRF24J40::Radio_t>()};
-    // std::vector<std::thread> threadVect;
+    
+    //std::vector<std::thread> threadVect;
     
 
 //std::cout << "\033[2J\033[H" << std::flush;
@@ -19,14 +19,26 @@ void Run_t::start(){
 // 
         // for (int i = 0; i < 4; ++i) {
             // threadVect.emplace_back([&mrf]() {
-            mrf->Run();
+           // mrf->Run();
             // });
         //}
 // 
         //Esperar a que todos los hilos terminen
         // for (auto& threadRun : threadVect) {
             // threadRun.join();
-        // }      
+        // }    
+
+
+
+auto mrf { std::make_unique<MRF24J40::Radio_t>()};
+
+        // Inicializar hilos y ejecutar las clases en paralelo
+        std::thread thread1(&MRF24J40::Radio_t::Run, mrf.get());
+    
+        // Esperar a que todos los hilos terminen
+        thread1.join();
+
+
       }
        catch(...){
          std::cerr<<"\nerror :(\n";
