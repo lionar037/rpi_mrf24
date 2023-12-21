1a66ec575c76e788393ac13f9c0db5a6a326afa9
bee <bee@macbook.local>
2023-12-21 19:48:11 -0300

    update 202312211948

--- src/spi/src/spi.h
+++ src/spi/src/spi.h
@@ -34,16 +34,16 @@ namespace SPI{
 
   private:
 
-    uint8_t tx_buffer[4]{0x00}  __attribute__((unused));
-    uint8_t rx_buffer[4]{0x00}  __attribute__((unused));
+    uint8_t tx_buffer[4]{0x00};//  __attribute__((unused));
+    uint8_t rx_buffer[4]{0x00};//  __attribute__((unused));
 
     const uint32_t len_data { 32 };
-    const uint32_t spi_speed { 0 }  __attribute__((unused));
+    const uint32_t spi_speed { 0 } ;// __attribute__((unused));
     
     int fs{0};
     int ret{0};
-    
-    std::unique_ptr<struct spi_ioc_transfer >spi{nullptr} __attribute__((unused));
+    std::unique_ptr<struct spi_ioc_transfer >spi;
+    //std::unique_ptr<struct spi_ioc_transfer >spi{nullptr} ;
     uint8_t looper{0};
     uint32_t scratch32{0};
   };
