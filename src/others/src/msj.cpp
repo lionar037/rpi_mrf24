83a7dec46ee15eb0ca8f65333795a70415fb0108
bee <bee@macbook.local>
2023-12-22 01:27:51 -0300

    update 202312220127

--- src/others/src/msj.cpp
+++ src/others/src/msj.cpp
@@ -3,15 +3,35 @@
 namespace DEVICES{
 
     
-
-    
+    void Msj_t::passMessage(QR_OLED_BUFF* qr_oled_msjp){
+        return ;
+    }
+        
     void Msj_t::get()
     {
-
+        return;
     }
 
     void Msj_t::set(std::string& msj){
-
+        return;
     }
 
+
+
+    
+    
+    void Msj_t::printData (bool flag , std::string_view msj){
+        return;
+    }
+    
+    
+    void Msj_t::printQr   (bool flag , std::string_view msj){
+        return;
+    }
+    
+    
+    void Msj_t::printOled (bool flag , std::string_view msj){
+        return;
+    }
+        
 }
--- src/others/src/msj.h
+++ src/others/src/msj.h
@@ -21,12 +21,18 @@ namespace DEVICES{
     struct Msj_t{
          Msj_t(){}
         ~Msj_t(){}
-        void get();
-        void set(std::string&);
-        void Start(){std::cout<<"Msj : Start() \n";}
+        void        get                 ( );
+        void        set                 ( std::string& );
+        void        Start               ( ){std::cout<<"Msj : Start() \n";}
+        void        passMessage         ( QR_OLED_BUFF* );
+        void        printData           ( bool , std::string_view );
+        void        printQr             ( bool , std::string_view );
+        void        printOled           ( bool , std::string_view );
+        
     private:
         std::string txt;
         QR_OLED_BUFF m_qr_oled_buff_msj;
+        bool flag { false };
     };
 
 
