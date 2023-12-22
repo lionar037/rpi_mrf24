5ccdd3702a09bbc72a3ed8a73f535b8bae106540
bee <bee@macbook.local>
2023-12-22 01:16:20 -0300

    update 202312220116

--- src/others/src/msj.h
+++ src/others/src/msj.h
@@ -1,8 +1,22 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <qr/src/qr.h>
+
+namespace QR{
+    //struct QR_OLED_BUFF;
+}
 
 namespace DEVICES{
+
+
+
+    // typedef struct qr_oled{
+            // int width;
+            // int height;
+            // bool* data = nullptr;
+    // }QR_OLED_BUFF;
+
 
     struct Msj_t{
          Msj_t(){}
@@ -12,6 +26,7 @@ namespace DEVICES{
         void Start(){std::cout<<"Msj : Start() \n";}
     private:
         std::string txt;
+        QR_OLED_BUFF m_qr_oled_buff_msj;
     };
 
 
