e05cfdf640fd01550580f82b3965f81ff572353a
bee <bee@macbook.local>
2023-12-22 01:29:23 -0300

    update 202312220129

--- src/others/src/msj.h
+++ src/others/src/msj.h
@@ -24,14 +24,14 @@ namespace DEVICES{
         void        get                 ( );
         void        set                 ( std::string& );
         void        Start               ( ){std::cout<<"Msj : Start() \n";}
-        void        passMessage         ( QR_OLED_BUFF* );
+        void        passMessage         ( QR::QR_OLED_BUFF* );
         void        printData           ( bool , std::string_view );
         void        printQr             ( bool , std::string_view );
         void        printOled           ( bool , std::string_view );
-        
+
     private:
         std::string txt;
-        QR_OLED_BUFF m_qr_oled_buff_msj;
+        QR::QR_OLED_BUFF m_qr_oled_buff_msj;
         bool flag { false };
     };
 
