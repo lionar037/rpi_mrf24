453e2e13fdd6729c1a685fe53ddfc379d9c7e682
bee <bee@macbook.local>
2023-12-22 02:14:08 -0300

    update 202312220214

--- src/display/src/oled.cpp
+++ src/display/src/oled.cpp
@@ -17,6 +17,8 @@ SSD1306 myOLED(myOLEDwidth , myOLEDheight) ; // instantiate  an object
 
 
 bool Oled_t::create(const std::string_view& textOnOled){
+
+        std::cout<<"texto : "<< textOnOled.data()<<"\n";
         static int Step { 0 };        
         uint8_t  screenBuffer[myOLEDwidth * (myOLEDheight/8)+1]; 
                 
