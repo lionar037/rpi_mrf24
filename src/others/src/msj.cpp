5a352de069981debbdf0fcef7f6b28efbe6ff149
bee <bee@macbook.local>
2023-12-22 02:10:28 -0300

    update 202312220210

--- src/others/src/msj.cpp
+++ src/others/src/msj.cpp
@@ -1,4 +1,5 @@
 #include <others/src/msj.h>
+#include<app/src/config.h>
 
 namespace DEVICES{    
     void Msj_t::passMessage(QR::QR_OLED_BUFF* qr_oled_msjp){
@@ -23,9 +24,10 @@ namespace DEVICES{
     }
         
     void Msj_t::printOled (bool flag , std::string_view msj){
-        
+        #ifdef USE_OLED2
         auto qr_oled{std::make_unique<QR::Qr_img_t>()};
         qr_oled->create(msj);
+        #endif
         return;
     }
         
