04d84f0100c477b0d5ba0ad3e4a655be174bd135
bee <bee@macbook.local>
2023-12-23 20:45:06 -0300

    update 202312232045

--- src/run/src/radio.h
+++ src/run/src/radio.h
@@ -78,7 +78,7 @@ namespace MRF24J40{
             #endif
         #endif             
         struct DATA::packet_tx                  buffer_transmiter{};
-        std::unique_ptr <GPIO::Gpio> gpio{};    
+        std::unique_ptr <GPIO::Gpio> gpio{nullptr};    
         static std::unique_ptr < SECURITY::Security_t >     security;
         static std::unique_ptr < MOSQUITTO::Mosquitto_t >   mosq;                    
     };
