66f9d8d5a437ec060652db18eae305712d4c8191
bee <bee@macbook.local>
2023-12-23 22:35:52 -0300

    update 202312232235

--- src/others/src/rfflush.cpp
+++ src/others/src/rfflush.cpp
@@ -29,7 +29,7 @@ static DEVICES::Msj_t msj;
     void Fflush_t::print(std::string_view str_txt, int row, int col) 
     { 
         #ifdef ENABLE_PRINTS_DBG  
-        std::string dat = "\033[" + row + ";" + col + "H" + str_txt.data();  
+        std::string dat = "\033[" + std::to_string(row) + ";" + std::to_string(col) + "H" + str_txt.data();  
         msj.set(dat.data());
         //std::cout << "\033[" << row << ";" << col << "H"<<str_txt.data();    
         #endif
