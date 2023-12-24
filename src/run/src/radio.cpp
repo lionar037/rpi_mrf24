ca27f4067506f9e3e12e5152812a69793165692e
bee <bee@macbook.local>
2023-12-23 23:06:36 -0300

    update 202312232306

--- src/run/src/radio.cpp
+++ src/run/src/radio.cpp
@@ -282,7 +282,8 @@ void Radio_t::handle_rx() {
         SET_COLOR(SET_COLOR_YELLOW_TEXT);
         std::cout<<temperatureToString.data(); 
         msj_txt=reinterpret_cast<const char*>(mrf24j40_spi.get_rxinfo()->rx_data) ;
-        monitor->
+        
+        monitor->maxLines(files);
         return;    
     }
 
