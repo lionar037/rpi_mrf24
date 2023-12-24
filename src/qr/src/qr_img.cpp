3ad6d94b8ccd16a37f82faf7096742470ae134b4
bee <bee@macbook.local>
2023-12-24 00:35:47 -0300

    update 202312240035

--- src/qr/src/qr_img.cpp
+++ src/qr/src/qr_img.cpp
@@ -141,8 +141,8 @@ static QR_OLED_BUFF codeQrGlobal;
         std::cout << "\033[" << "17" << ";" << "0" << "H" <<"\n";        
                 for (int y = 0; y < codeQrGlobal.height; y++) {
                 for (int x = 0; x < codeQrGlobal.width; x++)   {             
-                
-                   std::cout << (codeQrGlobal.data[y * codeQrGlobal.width + x] & 1 ? "::" : "  ");// std::cout << (qr->data[y * qr->width + x] & 1 ? "██" : "  ");
+                std::cout<<"::";
+                   //std::cout << (codeQrGlobal.data[y * codeQrGlobal.width + x] & true ? "::" : "  ");// std::cout << (qr->data[y * qr->width + x] & 1 ? "██" : "  ");
                 }
             std::cout << "\n";
             }
