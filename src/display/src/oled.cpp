60f6e7e5c84b5b9b8e3cd7a71f5c503bcd2ecc63
bee <bee@macbook.local>
2023-12-24 16:35:54 -0300

    update 202312241635

--- src/display/src/oled.cpp
+++ src/display/src/oled.cpp
@@ -147,7 +147,7 @@ void  Oled_t::Graphics(const int x,const int y,const bool* z,const uint8_t* w){
         for(int i=0 ; i<( y*x ); i++){
                 buff[l] |= (w[i] == '1' ? 1 : 0) << Position;                 
                 //std::cout<<  (w[i] & 1  ? "::" : "  ") ;                                                                           
-                std::cout<<  ((buff[l]>>Position  )& 1  ? "::" : "  ") ;  
+                std::cout<<  ((buff[l]>>Position  )& 1  ? "::" : " .") ;  
                 Position++; module ++;
                 if(module >=x){std::cout<<"\n"; module=0;}
                 if (Position >= 8) {Position=0;l++;}                                
