9c7426b625f990edf0116164f743a2b036e85b87
bee <bee@macbook.local>
2023-12-24 01:59:24 -0300

    update 202312240159

--- src/run/src/run.cpp
+++ src/run/src/run.cpp
@@ -64,11 +64,11 @@ void Run_t::start()
         if(flag==true){
                 //display->create(MRF24J40::msj_txt.c_str());
 
-                // const auto x = codeQrGlobal.height;
-                // const auto y = codeQrGlobal.width;
+                auto x = codeQrGlobal.height;
+                auto y = codeQrGlobal.width;
                 // const auto z = codeQrGlobal.data;
                 const bool d = true;
-                display->Graphics(10,11,d);
+                display->Graphics(x,y,d);
             }
         #endif                                
     }
