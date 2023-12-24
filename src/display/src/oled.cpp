9d5729f79abb13efad8fa9ed0c0fe36696d6bef4
bee <bee@macbook.local>
2023-12-24 04:43:43 -0300

    update 202312240443

--- src/display/src/oled.cpp
+++ src/display/src/oled.cpp
@@ -142,8 +142,8 @@ void  Oled_t::Graphics(const int x,const int y,const bool* z,const uint8_t* w){
         int l=0,Position=0;
         int module =0;
         static int move{0};
-        std::cout<<"imprime QR \n\n";
-        for(int i=0;i<y*x;i++){
+        std::cout<<"imprime QR : " << std::to_string(x*y)<<"\n\n";
+        for(int i=0 ; i<( y*x ); i++){
                 buff[l] |= (z[i] == '1' ? 1 : 0) << Position; 
                 //std::cout<<  (w[i] & 1  ? "::" : " .") ;                                           
                 std::cout<<"::";
