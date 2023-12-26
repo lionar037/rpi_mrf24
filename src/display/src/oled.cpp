04d96a44d5f33ecb4843e673ce7a1e9abac8943a
bee <bee@macbook.local>
2023-12-26 00:09:03 -0300

    update 202312260009

--- src/display/src/oled.cpp
+++ src/display/src/oled.cpp
@@ -142,20 +142,21 @@ void  Oled_t::Graphics(const int x,const int y,const bool* z,const uint8_t* w){
         
         std::cout << "\033[" << "15" << ";" << "0" << "H" <<"\n";  
         
-        for(int i=0 ; i< (y+3)*(x+3); i++){buff[l++]=0x00;}l=0;
+        for(int i=0 ; i< (y+3)*(x+3); i++){buff[l++]=0x00;}l=-1;
 
         for( int i=0 ; i<(y+2)*(x+2) ; )
         {                
                 if((!(i % 29) ) || Position==0)
+                //if((!(i % 29) && (i!=0) ) || Position==0)
                 {
                         l++;
                         Position=8;                                                                      
                 }
                 Position--; 
-                
+
                 if(i<(x*y)) buff[l] |= (w[i] & true ? 1 : 0) << Position ;                         
                 
-                //if((!(i % 29) && (i!=0) ) || Position==0)
+                
 
                            
                 i++;                                                                                
