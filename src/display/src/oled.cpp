60c8e4a501e0d3937424e9c8cc68c2d386ab1c13
bee <bee@macbook.local>
2023-12-25 23:34:32 -0300

    update 202312252334

--- src/display/src/oled.cpp
+++ src/display/src/oled.cpp
@@ -146,14 +146,14 @@ void  Oled_t::Graphics(const int x,const int y,const bool* z,const uint8_t* w){
 l=0;
         for(int i=0 ; i<( (y+3)*(x+3) ); i++){                
                 
-                buff[l] |=  (w[i] & true ? 1 : 0) << Position ;                         
+                if(i<(x*y))buff[l] |=  (w[i] & true ? 1 : 0) << Position ;                         
                 Position++;
                 
                 if((!(i%x)&& !0 ) || Position==8)
                 {
                         l++;
                         Position=0;                        
-                        //if(!(i%x))std::cout<<"\n";                        
+                                              
                 }                                                                            
         }
         
@@ -164,7 +164,7 @@ l=0;
         myOLED.buffer = (uint8_t*) &fullscreenBuffer; // buffer to the pointer
         myOLED.OLEDclearBuffer();  
         static int move{0};
-        myOLED.OLEDBitmap(move++, 0 , x+3, y+3, buff, false);        
+        myOLED.OLEDBitmap(move++, 0 , x+3, y, buff, false);        
         //myOLED.OLEDBitmap(move++, 0 , 64, 64, bigImage, false);
         //myOLED.OLEDBitmap(move++, 0 , 32, 32, cuadrado, false);
         
