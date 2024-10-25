330ff323642c5025a50c00c874199d3ac8dec398
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-25 02:50:14 -0300

    update 202410250250

--- mrf24_prj/run/src/radio.cpp
+++ mrf24_prj/run/src/radio.cpp
@@ -269,8 +269,8 @@ void Radio_t::handle_rx() {
         
         //auto bs = (~packet_data_tmp->size)&0xffff;//ver cual es la correcta ~ o no 
         
-        monitor->print("buffer_receiver->size : " + reinterpret_cast<const int *>(bs) + "\n" ,files++,col);
-        monitor->print("data_receiver->data : " + reinterpret_cast<const char *>(packet_data_tmp->data) + "\n" ,files++,col);
+        //monitor->print("buffer_receiver->size : " + reinterpret_cast<const int *>(bs) + "\n" ,files++,col);
+        //monitor->print("data_receiver->data : " + reinterpret_cast<const char *>(packet_data_tmp->data) + "\n" ,files++,col);
         monitor->print("\nbuff: \n" + buff ,files++,col);
         monitor->print("\r\n" ,files++,col);
     #endif            
