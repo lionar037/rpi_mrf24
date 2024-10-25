75cee485b478b81c6176be0b4b5493579cb030d2
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-25 00:38:15 -0300

    update 202410250038

--- project/app/include/config.h
+++ project/app/include/config.h
@@ -64,14 +64,15 @@
 
 // Prints Debugger
 
-//#define DBG//DBG_GPIO
-//#define DBG_BUFFER
-//#define DBG_GPIO //DBG_GPIO
-//#define DBG_FILES //imprime Debugger en files
-//#define DBG_DISPLAY_OLED
-//#define DBG_MRF
-//#define ENABLE_PRINTS_DBG
-//#define DBG_PRINT_GET_INFO
+#define DBG//DBG_GPIO
+#define DBG_BUFFER
+#define DBG_GPIO //DBG_GPIO
+#define DBG_FILES //imprime Debugger en files
+#define DBG_DISPLAY_OLED
+#define DBG_MRF
+#define ENABLE_PRINTS_DBG
+#define DBG_PRINT_GET_INFO
+#define DBG_GPIO
 
 //config QR string 
 //Linea de configuracion para codigo Qr de una red wifi
