f807bc7da7080923b793c118131337b3c8a321e4
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-25 22:38:25 +0100

    update 202410252238

--- mrf24_prj/network/include/mosquitto.h
+++ mrf24_prj/network/include/mosquitto.h
@@ -5,7 +5,7 @@
 
 #ifdef ENABLE_MOSQUITTO    
     //#redefine HOST_SERVER_MOSQUITTO "192.168.1.41"
-    //#define HOST_SERVER_MOSQUITTO "raspberry.local"
+    #define HOST_SERVER_MOSQUITTO "raspberry.local"
 #else 
     #define HOST_SERVER_MOSQUITTO "mosquitto.local"
 #endif
