82d6895798a76dd56651739e040c9672101de2cb
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-26 18:02:41 +0100

    update 202410261802

--- mrf24_prj/gpio/src/gpio.cpp
+++ mrf24_prj/gpio/src/gpio.cpp
@@ -1,3 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////
+//     
+//          filename            :   gpio.cpp
+//          License             :   GNU 
+//          Author              :   Lio
+//          Change History      :
+//          Processor           :   ARM
+//          Hardware            :		
+//          Complier            :   ARM
+//          Company             :
+//          Dependencies        :
+//          Description         :
+//          brief               :	
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <gpio/include/gpio.h>
+#include <app/include/config.h>
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <filesystem>
+#include <unistd.h> 
+#include <cstring>
+
 extern "C"{
     #include <stdio.h>
     #include <stdlib.h>
@@ -7,133 +35,196 @@ extern "C"{
     #include <fcntl.h>
     #include <poll.h>
 }
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <string_view>
-#include <string>
-#include <gpio/include/gpio.h>
-#include <app/include/config.h>
 
 
-namespace GPIO{
-    /*      HELPER FUNCTIONS       */
-    // FILE OPERATION
-    int Gpio_t::file_open_and_write_value(const std::string_view fname, const std::string_view wdata)
-    {
-        //@params : verifica que exista el pin . Si no existe ,retorna -1 .
-        const int fd = open(fname.data(), O_WRONLY | O_NONBLOCK);        
-        if (fd < 0)
-  //      {
-            printf("Could not open file %s...%d\r\n", fname.data(), fd);
-            //return -1;
-//        }
-        write(fd, wdata.data(), strlen(wdata.data()));        
-        close(fd);
+namespace GPIO {
+
+    Gpio_t::Gpio_t(bool& status)
+        : m_state(status), m_gpio_in_fd(-1) {
+        #ifdef DBG_GPIO
+        std::cout << "Gpio_t::Gpio_t()\n";
+        #endif
+    }
+
+    Gpio_t::~Gpio_t() {
+        CloseGpios();
+        #ifdef DBG_GPIO
+        std::cout << "~Gpio()\n";
+        #endif
+    }
+
+    int Gpio_t::file_open_and_write_value(std::string_view fname, std::string_view wdata) {
+        std::ofstream file(fname.data(), std::ios::out | std::ios::trunc);
+        if (!file.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "El archivo no existe o no se pudo abrir : " << fname << std::endl;
+            #endif
+            return -1;
+        }
+        file << wdata;
         return 0;
     }
 
+    int Gpio_t::gpio_export(int gpio_num) {
 
-    //  int Gpio_t::file_open_and_write_value(const std::string_view fname, const std::string_view wdata) {
-        // std::ofstream file(fname.data(), std::ios::out | std::ios::binary);
-        // if (!file.is_open()) {
-            // std::cerr << "Error: Could not open file " << fname.data() << std::endl;
-            // return -1;
-        // }
-// 
-        // file.write(wdata.data(), static_cast<std::streamsize>(wdata.length()));
-        // file.close();
-// 
-        // return 0;
-    // }
-
-    // GPIO EXPORT
-    int Gpio_t::gpio_export(const int gpio_num)
-    {
-        char gpio_str[4];
-        sprintf(gpio_str, "%d", gpio_num);
-          //std::cout<<"gpio_export\n";
-        return file_open_and_write_value(SYSFS_GPIO_PATH SYSFS_GPIO_EXPORT_FN,std::to_string(gpio_num) /*gpio_str*/);
+        #ifdef DBG_GPIO
+        std::cout << "gpio_export()\n";
+        #endif
+        //std::string str = SYSFS_GPIO_PATH.data()  SYSFS_GPIO_EXPORT_FN.data() ;
+         std::string path = std::string(SYSFS_GPIO_PATH) + std::string(SYSFS_GPIO_EXPORT_FN); 
+        return file_open_and_write_value( path  , std::to_string(gpio_num));
     }
 
-    // GPIO UNEXPORT
-    int Gpio_t::gpio_unexport(const int gpio_num)
-    {
-        char gpio_str[6];
-        //    char gpio_str[4];
-        sprintf(gpio_str, "%d", gpio_num);
-        //std::cout<<"dbg gpio_unexport "<< gpio_num <<"\n";
-        return file_open_and_write_value(SYSFS_GPIO_PATH SYSFS_GPIO_UNEXPORT_FN, std::to_string(gpio_num) /*gpio_str*/);
+    int Gpio_t::gpio_unexport(int gpio_num) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_unexport()\n";
+        #endif
+        std::string path = std::string(SYSFS_GPIO_PATH) + std::string(SYSFS_GPIO_EXPORT_FN); 
+        return file_open_and_write_value( path  , std::to_string(gpio_num));
     }
 
-    // GPIO DIRECTION
-    int Gpio_t::gpio_set_direction(const int gpio_num, const std::string_view dir)
-    {
-        char path_str[64];
-        sprintf(path_str, "%s/gpio%d%s", SYSFS_GPIO_PATH, gpio_num, SYSFS_GPIO_DIRECTION);
-        //std::cout<<"gpio_set_direction\n";
-        return file_open_and_write_value(path_str, dir.data());
+    int Gpio_t::gpio_set_direction(int gpio_num, std::string_view dir) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_direction() : " << dir << std::endl;
+        #endif
+        std::string path = std::string(SYSFS_GPIO_PATH) +"/gpio" +  std::to_string(gpio_num) + std::string(SYSFS_GPIO_DIRECTION); 
+        return file_open_and_write_value( path, dir);
     }
 
-    // GPIO SET VALUE
-    int Gpio_t::gpio_set_value(const int gpio_num, const std::string_view value)
-    {
-        char path_str[64];
-        sprintf(path_str, "%s/gpio%d%s", SYSFS_GPIO_PATH, gpio_num, SYSFS_GPIO_VALUE);
-        //std::cout<<"gpio_set_value\n";
-        // DBG_GPIO_PRINT(8);
-        return file_open_and_write_value(path_str, value.data());
+    int Gpio_t::gpio_set_value(int gpio_num, std::string_view value) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_value()\n";
+        #endif
+        std::string path = std::string (SYSFS_GPIO_PATH) +  "/gpio" + std::to_string(gpio_num) + std::string (SYSFS_GPIO_VALUE);
+        return file_open_and_write_value( path , value);
     }
 
-    // GPIO SET EDGE
-    int Gpio_t::gpio_set_edge(const int gpio_num, const std::string_view edge)
-    {
-        char path_str[64];
-        sprintf(path_str, "%s/gpio%d%s", SYSFS_GPIO_PATH, gpio_num, SYSFS_GPIO_EDGE);
-        //std::cout<<"gpio_set_edge\n";
-        return file_open_and_write_value(path_str, edge.data());
+    int Gpio_t::gpio_set_edge(int gpio_num, std::string_view edge) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_set_edge()\n";
+        #endif
+        std::string path = std::string (SYSFS_GPIO_PATH)  + "/gpio" + std::to_string(gpio_num) + std::string (SYSFS_GPIO_EDGE);
+        return file_open_and_write_value( path , edge);
     }
 
-    int Gpio_t::gpio_get_fd_to_value(const int gpio_num)
-    {
-        //int fd;
-        char fname[64];
-        sprintf(fname, "%s/gpio%d%s", SYSFS_GPIO_PATH, gpio_num, SYSFS_GPIO_VALUE);
-        const int fd = open(fname, O_RDONLY | O_NONBLOCK);
-        //printf("name : %s\n",fname);
-        if (fd < 0)
-        {
-            printf("Could not open file %s... %d\r\n", fname, fd);
+    int Gpio_t::gpio_get_fd_to_value(int gpio_num) {
+        #ifdef DBG_GPIO
+        std::cout << "gpio_get_fd_to_value()\n";
+        #endif
+        const std::string fname = "/sys/class/gpio/gpio" + std::to_string(gpio_num) + "/value";
+        if (!std::filesystem::exists(fname)) {
+            #ifdef DBG_GPIO
+            std::cerr << "El archivo " << fname << " no existe." << std::endl;
+            #endif
+            return -1;
         }
-        return fd;
+        std::ifstream file(fname);
+        if (!file.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "No se pudo abrir el archivo " << fname << " para leer." << std::endl;
+            #endif
+            return -1;
+        }
+        std::string contenido((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        #ifdef DBG_GPIO
+        std::cout << "Contenido actual del archivo " << fname << ":\n" << contenido << std::endl;
+        #endif
+        return std::stoi(contenido);
     }
 
+    void Gpio_t::CloseGpios() {
+        if (filenameGpio.is_open()) filenameGpio.close();
+        if (m_gpio_in_fd != -1) {close(m_gpio_in_fd);}
+        gpio_set_value(m_gpio_out, VALUE_LOW);
+        gpio_unexport(m_gpio_out);
+        gpio_unexport(m_gpio_in);
+        #ifdef DBG_GPIO
+        std::cout << "Gpio_t::CloseGpios()" << std::endl;
+        #endif
+    }
 
+    int Gpio_t::digitalWrite(uint16_t pin, std::string_view st) {
+        #ifdef DBG_GPIO
+        std::cout << "digitalWrite : " << pin << " type : " << st << std::endl;
+        #endif
+        return gpio_set_value(pin, st);
+    }
 
-    bool Gpio_t::settings(const int pin , const std::string_view str_v ,std::ifstream& fileTmp){
-        const std::string filePathGpio = "/sys/class/gpio/gpio" + std::to_string(pin) + "/direction";                                
-        const std::string fNameResult ="echo " + std::to_string(pin) + " > /sys/class/gpio/export";               
-        fileTmp.open(filePathGpio.c_str());        
-        if(!fileTmp.is_open()){
+    int Gpio_t::pinMode(uint16_t number_gpio, std::string_view read_direction) {
+        #ifdef DBG_GPIO
+        std::cout << "pinMode : " << number_gpio << " Direction : " << read_direction << std::endl;
+        #endif
+        return gpio_set_direction(number_gpio, read_direction);
+    }
 
-            const int result_output = std::system(fNameResult.c_str());
-            if (result_output == 0) {
-                #ifdef DBG_GPIO
-                    std::cout << "Pin GPIO "+ std::to_string(pin) +" exported successfully." << "\n";
-                #endif
-            } else {
-                #ifdef DBG_GPIO
-                    std::cerr << "Error unexporting GPIO "+ std::to_string(pin) +"." <<"\n";
-                #endif
-                return false;
+    int Gpio_t::digitalRead(int gpio) {
+        #ifdef DBG_GPIO
+        std::cout << "digitalRead: " << gpio << std::endl;
+        #endif
+        return gpio_get_fd_to_value(gpio);
+    }
+
+    int Gpio_t::settings(int gpio, std::string_view direction, std::ifstream& filenameTmp) {
+        filenameTmp.open("/sys/class/gpio/gpio" + std::to_string(gpio) + "/direction");
+        if (!filenameTmp.is_open()) {
+            #ifdef DBG_GPIO
+            std::cerr << "No se pudo abrir el archivo de dirección para el GPIO " << gpio << std::endl;
+            #endif
+            return -1;
+        }
+        return 0;
+    }
+
+    int Gpio_t::getNextId() {
+        int max_id = -1;
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::getNextId()" << std::endl;
+        #endif
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos a gpioPtr
+            if (gpioPtr->ID > max_id) {  // Cambiar a ->
+                max_id = gpioPtr->ID;  // Cambiar a ->
             }
-        }  
-            fileTmp.close();  
-            gpio_unexport(pin);        
-            gpio_export(pin);             
-            gpio_set_direction(pin,str_v.data());
-        return true;
+        }
+        return max_id + 1;
     }
+
+    void Gpio_t::updateGpioMaps() {
+
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::updateGpioMaps()" << std::endl;
+        #endif
+        gpioById.clear();
+        gpioByPin.clear();
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos a gpioPtr
+            gpioById[gpioPtr->ID] = gpioPtr.get();  // Cambiar a ->
+            gpioByPin[gpioPtr->gpio] = gpioPtr.get();  // Cambiar a ->
+        }
+    }
+
+
+    void Gpio_t::printGpios() const {
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::printGpios()" << std::endl;
+        #endif
+        for (const auto& gpioPtr : m_gpio_cfg) {  // Cambiamos el nombre de la variable a gpioPtr
+            const GpioConform_t& gpio = *gpioPtr;  // Desreferenciamos el puntero
+            std::cout << "ID: " << gpio.ID << ", GPIO: " << gpio.gpio << ", Direction: " << gpio.dir 
+                      << ", Edge: " << gpio.edge << ", Value: " << gpio.value << "\n";
+        }
+    }
+
+    void Gpio_t::addGpio(uint16_t gpio_pin, std::string dir, std::string edge, std::string value) {
+        #ifdef DBG_GPIO
+            std::cout << "Gpio_t::addGpio" << std::endl;
+        #endif
+
+        int id = getNextId();
+        auto gpio = std::make_unique<GpioConform_t>(id, gpio_pin, std::move(dir), std::move(edge), std::move(value), true);
+        m_gpio_cfg.push_back(std::move(gpio)); // Usa std::move aquí
+        gpioById[id] = m_gpio_cfg.back().get(); // Almacena el puntero a la última entrada
+        gpioByPin[gpio_pin] = m_gpio_cfg.back().get(); // Almacena el puntero a la última entrada
+    }
+
 
     const bool Gpio_t::app(bool& flag) 
     {
@@ -144,6 +235,10 @@ namespace GPIO{
         int m_looper { 0 };
         char *buf[64];
 
+        #ifdef DBG_GPIO
+            printf("const bool Gpio_t::app()...%d\r\n",m_res);   
+        #endif         
+
         settings( m_gpio_in  , DIR_IN  ,filenameGpio);
         settings( m_gpio_out , DIR_OUT ,filenameGpio);
         
@@ -152,7 +247,6 @@ namespace GPIO{
 
           
         m_gpio_in_fd = gpio_get_fd_to_value(m_gpio_in);
-        //m_gpio_in_fd = gpio_in_fd;
         // We will wait for button press here for 10s or exit anyway
         if(m_state==true)
         {
@@ -189,43 +283,6 @@ namespace GPIO{
             std::this_thread::sleep_for(std::chrono::milliseconds(100));            
         }    
         gpio_set_value(m_gpio_out,VALUE_LOW);
-        
-   
         return false;
-    }
-
-    void Gpio_t::CloseGpios()
-    {
-        if(filenameGpio.is_open())filenameGpio.close();   
-        close(m_gpio_in_fd);        
-        gpio_set_value(m_gpio_out,VALUE_LOW);
-     
-        gpio_unexport(m_gpio_out);
-        gpio_unexport(m_gpio_in);
-        //if(fileGpioOutput.is_open())fileGpioOutput.close();
-    }
-
-    Gpio_t::~Gpio_t(){
-            
-            CloseGpios();            
-            
-        #ifdef DBG
-            std::cout<<"~Gpio_t()\n";
-        #endif       
-    }
-
-
-    int Gpio_t::digitalWrite(const uint16_t pin, std::string_view st) {
-        #ifdef DBG_GPIO
-        std::cout << "digitalWrite : " << pin << " type : " << st << std::endl;
-        #endif
-        return gpio_set_value(pin, st);
-    }
-
-    int Gpio_t::pinMode(const uint16_t number_gpio, std::string_view read_direction) {
-        #ifdef DBG_GPIO
-        std::cout << "pinMode : " << number_gpio << " Direction : " << read_direction << std::endl;
-        #endif
-        return gpio_set_direction(number_gpio, read_direction);
     }
 }
