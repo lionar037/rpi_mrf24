8c9715e8003d5bee28eadfb0fe706282448b4edf
lionar037 <8897224+lionar037@users.noreply.github.com>
2024-10-26 06:34:44 +0100

    actuzalizacion desde raspberry pi 4

--- mrf24_prj/display/src/oled.cpp
+++ mrf24_prj/display/src/oled.cpp
@@ -213,12 +213,48 @@ namespace OLED{
 		myOLED.OLEDupdate();
 	}
 
-	void Oled_t::Graphics(const int pos_x,const int pos_y ,const bool status , uint8_t* p_qr_msj){		
-		myOLED.setCursor(pos_x, pos_y);
-		for(uint16_t x=0 ;x < pos_x;++x)
-			for(uint16_t y=0 ;y < pos_y;++y){
-				//myOLED.drawPixel(x,y,*p_qr_msj++);
-			}			
-		//myOLED.OLEDupdate();	
-	}
+	//void Oled_t::Graphics(const int pos_x,const int pos_y ,const bool status , uint8_t* p_qr_msj){		
+	//	myOLED.setCursor(pos_x, pos_y);
+	//	for(uint16_t x=0 ;x < pos_x;++x)
+	//		for(uint16_t y=0 ;y < pos_y;++y){
+	//			//myOLED.drawPixel(x,y,*p_qr_msj++);
+	//		}			
+	//	//myOLED.OLEDupdate();	
+	//}
+	 void  Oled_t::Graphics(const int x,const int y,const bool* z,const uint8_t* w){
+                uint8_t buff [(x+3)*(y+3)]={0x00};
+                int l{-1},Position{0};
+                //int module =0;
+
+                //std::cout << "\033[" << "15" << ";" << "0" << "H" <<"\n";  
+
+                //for(int i=0 ; i< (y+3)*(x+3); i++){buff[l++]=0x00;}
+                l=-1;
+
+                for( int i=0 ; i<(y)*(x) ; )
+                {                
+                        if((!(i % 29) ) || Position==0)                
+                        {
+                                l++;
+                                Position=8;                                                                      
+                        }
+                        Position--; 
+                        //if(i<(x*y)) 
+                        buff[l] |= (w[i] & true ? 1 : 0) << Position ;                                                                                    
+                        i++;                                                                                
+                }        
+                uint8_t fullscreenBuffer[1024]; 
+                myOLED.buffer = (uint8_t*) &fullscreenBuffer; // buffer to the pointer
+                myOLED.OLEDclearBuffer();  
+                static int move{0};
+                
+                if(move==32)move=0;
+
+                myOLED.OLEDBitmap(move++, 0 , x, y, buff, false);                
+                myOLED.setCursor(128-24, 64-12);
+                myOLED.setFontNum(OLEDFontType_Wide);
+                myOLED.print(reinterpret_cast<int>(count++));                
+                myOLED.OLEDupdate();
+        }
+
 }
